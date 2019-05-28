cd27adee197019286ea98564d7c2498e3701ea0f
Klaus Schwartz <Klaus Schwartz>
2019-05-28 20:46:45 +0200

    should work now

--- ami2/jenkins.sh
+++ ami2/jenkins.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # Start Jenkins and connect a volume to persist all jenkins data.
 
-echo "This script will install Jenkins as Docker Container and map the configuration to a folder on you Harddrive"
 echo ""
+echo ""
+echo "This script will install Jenkins as Docker Container and map the configuration to a folder on you Harddrive"
 echo ""
 echo ""
 
 echo "Which Port should Jenkins use to be reached from the outside?"
 read -p 'Port (no Input = 8080): ' jport
+
 if [ -z "$jport" ]
 then
 #empty input
 jport="8080"
 fi
-echo OK, Jenkins will be available on Port $jport
 
+echo ""
+echo OK, Jenkins will be available on Port $jport
+echo ""
+echo ""
 
 echo "Please enter a path for the jenkins volume, where Jenkins should store configuration data"
 echo "( be carefull, this directory is accessable out of jenkins and insecure )"
@@ -24,7 +29,10 @@ then
 #empty input
 jpath="~/v_jenkins"
 fi
+echo ""
 echo OK, Jenkins configuration data will be stored in $jpath
+echo ""
+echo ""
 
 echo "Are you sure to install Jenkins with these parameters?"
 read -p 'Install Jenkins? (y/n) ' -n 1 -r

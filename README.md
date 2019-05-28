92bbf5da2a9097b623f056efadba7d44d9fcb80c
Klaus Schwartz <Klaus Schwartz>
2019-05-28 19:42:13 +0200

    step 2 - install dev tools

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ curl -sSL http://bit.ly/2ysbOFE | bash -s
 
 to make a new ami2 ec2 instance dev ready use:
 curl -sSL https://raw.githubusercontent.com/KlausSchwartz/quickstart/master/ami2/makeAmiDevReady.sh | bash -s
+
+
+# for development use:
+curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/KlausSchwartz/quickstart/ami-start/ami2/makeAmiDevReady.sh | bash -s
+(while ami-start is the branch name, where you have pushed your changes)
--- ami2/makeAmiDevReady.sh
+++ ami2/makeAmiDevReady.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-echo "###########################"
+echo "####################################"
 echo " step 1: update everything"
 echo " with 'sudo yum update -y'   "
-echo "##########################"
+echo "####################################"
 
 #step 1
-sudo yum update -y
+#sudo yum update -y
 
+echo "####################################"
+echo " step 2: install Development Tools"
+echo " with 'sudo yum update -y'   "
+echo "####################################"
+
+#step 2
+sudo yum groupinstall "Development Tools" -y

dc7a4f01b49ef133b8245f6dee90dd0fc526600b
Klaus Schwartz <Klaus Schwartz>
2019-05-28 20:39:40 +0200

    v1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ to make a new ami2 ec2 instance dev ready use:
 curl -sSL https://raw.githubusercontent.com/KlausSchwartz/quickstart/master/ami2/makeAmiDevReady.sh | bash -s
 
 
-# for development use:
+# for development use (with disabled cache):
 curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/KlausSchwartz/quickstart/ami-start/ami2/makeAmiDevReady.sh | bash -s
 
 (while "ami-start" is the branch name, where you have pushed your changes)
---
+++ jenkins.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Start Jenkins and connect a volume to persist all jenkins data.
+
+echo "This script will install Jenkins as Docker Container and map the configuration to a folder on you Harddrive"
+echo ""
+echo ""
+echo ""
+
+echo "Which Port should Jenkins use to be reached from the outside?"
+read -p 'Port (no Input = 8080): ' jport
+if [ -z "$jport" ]
+then
+#empty input
+jport="8080"
+fi
+echo OK, Jenkins will be available on Port $jport
+
+
+echo "Please enter a path for the jenkins volume, where Jenkins should store configuration data"
+echo "( be carefull, this directory is accessable out of jenkins and insecure )"
+read -p 'Path (no Input = "~/v_jenkins") ' jpath
+if [ -z "$jpath" ]
+then
+#empty input
+jpath="~/v_jenkins"
+fi
+echo OK, Jenkins configuration data will be stored in $jpath
+
+echo "Are you sure to install Jenkins with these parameters?"
+read -p 'Install Jenkins? (y/n) ' -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+    #create jpath
+    sudo mkdir -p $jpath
+
+    #set permissions for jpath (need a better solution here!)
+    sudo chmod 777 $jpath
+
+    #install jenkins
+    docker run -p 8080:$jport -p 50000:50000 -v $jpath:/var/jenkins_home jenkins/jenkins:lts
+
+fi
